bbc3f1022d38f76ab990dbc81573b165165d460c
Alexander Schuneman <alex_schuneman@outlook.com>
2017-09-26 21:32:10 +0100

    feat: Defined the state tree for the app
    State Tree is a map with a key 'movies'.
    The key contains a list of movies, each
    time a new list is created it points to
    this key.

--- voting-server/tests/immutable_spec.js
+++ voting-server/tests/immutable_spec.js
@@ -18,5 +18,31 @@ describe('immutabilty', () => {
         });
     });
 
+    describe(' a tree', () => {
 
+        function addMovie(currentState, movie) {
+            return currentState.set('movies', currentState.get('movies').push(movie));
+        }
+
+        it('is immutable', () => {
+            let state = Map({
+                movies: List.of('Trainspotting', '28 Days Later')
+            });
+            let nextState = addMovie(state, 'Sunshine');
+
+            expect(nextState).to.equal(Map({
+                movies: List.of(
+                    'Trainspotting',
+                    '28 Days Later',
+                    'Sunshine'
+                )
+            }));
+            expect(state).to.equal(Map({
+                movies: List.of(
+                    'Trainspotting',
+                    '28 Days Later'
+                )
+            }));
+        });
+    });
 });
